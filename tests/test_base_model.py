17c9a0e7157f9b2ac00fdfd3cbdbb79d77c44eea
Kenichiro Nishioka <kenichiro.nishioka@bebit.com>
2020-07-06 09:13:10 +0900

    Add pyproject.toml for black

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -2,5 +2,5 @@ from fifteen_five.models import BaseModel
 
 
 def test_valid_args():
-    BaseModel.valid_keys = ['key1']
-    assert BaseModel.valid_args({'key1': 'val1', 'key2': 'val2'}) == {'key1': 'val1'}
+    BaseModel.valid_keys = ["key1"]
+    assert BaseModel.valid_args({"key1": "val1", "key2": "val2"}) == {"key1": "val1"}
