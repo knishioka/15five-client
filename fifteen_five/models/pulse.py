4ff97a2a26d038b04867bedf736720bcc04ddaa4
Kenichiro Nishioka <kenichiro.nishioka@bebit.com>
2020-09-05 12:55:51 +0900

    Call User and Report models from Pulse model

--- fifteen_five/models/pulse.py
+++ fifteen_five/models/pulse.py
@@ -3,10 +3,16 @@
 from dateutil.parser import parse
 
 from .base_model import BaseModel
+from .report import Report
+from .user import User
 
 
 class Pulse(BaseModel):
-    """Pulse model."""
+    """Pulse model.
+
+    Examples:
+        >>> pulses = Pulse.all(params={"created_on_start": "2020-09-01", "created_on_end": "2020-09-06"})
+    """
 
     api_path = BaseModel.api_base_path + "pulse/"
     valid_keys = ["id", "report", "user", "create_ts", "value"]
@@ -23,7 +29,17 @@ class Pulse(BaseModel):
 
         """
         self.id = id
-        self.report = report
-        self.user = user
+        self._report = report
+        self._user = user
         self.create_ts = parse(create_ts)
         self.value = int(value)
+
+    @property
+    def user(self):
+        """Reported user."""
+        return User.find_by_url(self._user)
+
+    @property
+    def report(self):
+        """Pulse report."""
+        return Report.find_by_url(self._report)
